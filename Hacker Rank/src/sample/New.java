f895d8dc1a6d8c34a39fa557d7ecb0054a125cb8
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-06-20 14:37:15 +0530

    Add files via upload

---
+++ Hacker Rank/src/sample/New.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.util.Scanner;
+public class New
+{
+    public static void main(String[]arg){
+        System.out.println("Enter Two Numbers");
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b= sc.nextDouble();
+
+        System.out.println("Enter an Opertaor in one of them( + ");
+        char operator = sc.next().charAt(0);
+        double result;
+        switch (operator)
+        {
+            case '+':
+                result = a + b;
+                break;
+            default:
+                System.out.println("Entered operator is Incorrect");
+                return;
+        }
+
+        System.out.println("the result is  " + a + operator  + b +  "=" + result);
+    }
+}
